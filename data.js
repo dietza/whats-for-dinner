87b53fdec96ebf0e8647dabedf6a698a3e8d3228
Allison Dietz <dietz.allison@outlook.com>
2020-09-07 14:08:50 -0600

    Fix linking project files

---
+++ data.js
@@ -0,0 +1,20 @@
+var sides = [
+  'Spicy Brussel Sprouts',
+  'Roast Potatoes',
+  'Fried Avocado',
+  'Curried Zucchini Fritters',
+  'Miso Glazed Carrots',
+  'Garden Salad',
+  'Sweet Potato Fries',
+  'Coconut Rice',
+  'Garlic Butter Mushrooms'
+];
+
+var mainDishes = [
+  'Minestrone Soup',
+  ''
+];
+
+var desserts = [
+
+]
--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-// variables for querySelectors:
+
+// var sides = [
+//   'brussel sprouts',
+//   'roast potatoes',
+//   'fried avocado',
+//   'curried zucchini fritters'
+// ];
+
+
+// radio button selectors:
+var sideRadio = document.querySelector('#side');
+var mainRadio = document.querySelector('#main-dish');
+var dessertRadio = document.querySelector('#dessert');
+
+
+// buttons:
+var addRecipeButton = document.querySelector('.add-recipe-button');
+var letsCookButton = document.querySelector('.lets-cook-button');
+
+
+// sections:
+var cookPotIcon = document.querySelector('.cookpot-icon');
+
+
+
+// form inputs:
+
+
+
+// event listeners:
+addRecipeButton.addEventListener('click',addRecipe);
+
+
+// functions:
+
+function getRandomIndex(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function addRecipe() {
+
+}
+
+console.log(getRandomIndex(sides));
